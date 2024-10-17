7dabc82a13f9982b18700af74342e89ca13393f8
tooncool64 <mdallytc64@gmail.com>
2024-10-17 14:37:58 -0700

    Fix last thing

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e171f8ac8c853dc625a48c73015deb659d72aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b0abc76486f37f1c2ca2de55ddcba4ddd44f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
