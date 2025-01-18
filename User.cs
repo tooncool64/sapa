2d9c98c7ef70851852ebf534c1ea326c3748428b
tooncool64 <mdallytc64@gmail.com>
2025-01-18 14:30:45 -0800

    Continuing to fix Database Testing

--- User.cs
+++ User.cs
@@ -1,7 +1,11 @@
-﻿namespace BlazorApp;
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorApp;
 
 public class User
 {
+    [Required]
     public string Username;
+    
+    [Required]
     public string Password;
 }
