f5af470ea16b6fc4a742295a1c57b9f4cc06a6d4
tooncool64 <mdallytc64@gmail.com>
2025-01-31 09:51:42 -0800

    minor chnages

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3119de4b5164956c6fef68c679245c1f746c6c0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+627b755367c01f24406a1f84268acb12e99ac1b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
