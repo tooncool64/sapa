11f237b3a57ac71b48a4ec6838cbe5a82b44c3d6
tooncool64 <mdallytc64@gmail.com>
2025-01-31 00:18:42 -0800

    .

--- Program.cs
+++ Program.cs
@@ -64,7 +64,7 @@ builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefa
 
 builder.Services.AddAntiforgery(options => options.HeaderName = "X-CSRF-TOKEN");
 
-builder.Services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
