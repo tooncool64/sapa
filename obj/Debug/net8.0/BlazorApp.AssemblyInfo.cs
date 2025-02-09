f0780837211963a0cb22fdc763ee36c0893d4c1c
Diego Alvarez <alfaman360@gmail.com>
2025-02-09 13:02:56 -0800

    Appeal from spanish v1

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae15b45944e6a51e4ff01abe7cc5380b09176902")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e0c72465ea0876a78f0023fb25fa919d70d3c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
