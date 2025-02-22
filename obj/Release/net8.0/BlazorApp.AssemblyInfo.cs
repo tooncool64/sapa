5d735f036f8bcf5016fb01126d1fc5879705c281
tooncool64 <mdallytc64@gmail.com>
2025-02-22 15:54:35 -0800

    forced

--- obj/Release/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Release/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
