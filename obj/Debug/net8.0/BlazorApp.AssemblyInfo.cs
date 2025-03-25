80ee17046c10bb0703f5059011a69c7eaec93b24
tooncool64 <mdallytc64@gmail.com>
2025-03-25 10:13:18 -0700

    Removed authentication for user tests

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def4161ff0ea795e117f1d1cd708686930f63368")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45a861fee1817e68f89673067330b36447e73c85")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
