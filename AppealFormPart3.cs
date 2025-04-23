70ce92a1736294bb340a78a4b78cacf5a695cb83
tooncool64 <mdallytc64@gmail.com>
2025-04-22 23:34:29 -0700

    moar updates

---
+++ AppealFormPart3.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp;
+
+public class AppealFormPart3
+{
+
+    [Required(ErrorMessage = "Appeal explanation is required")]
+    public string AppealExplanation { get; set; } = string.Empty;
+
+}
