a8840987cba6b7642a589f9c2e1a1306699bca45
tooncool64 <mdallytc64@gmail.com>
2025-05-02 13:30:01 -0700

    supporting documentation edits.

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a09d259e71beed23c6c5a4a1d7e932b6949e17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8100f0048c1ba09a67c3d97e67100196348dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
